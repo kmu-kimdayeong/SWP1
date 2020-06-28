adae87d1ab064574492801a56177f602fe98b956
kmu-kimdayeong <chapchap1313@kookmin.ac.kr>
2020-06-28 19:53:31 +0900

    issue #7 change calc.py for error handling

--- calc/calc.py
+++ calc/calc.py
@@ -7,11 +7,11 @@ def application(environ, start_response):
 	first_num = d.get('first_num', [''])[0]
 	second_num = d.get('second_num', [''])[0]
 	sum, mul = 0, 0
-	if first_num.isdigit() and second_num.isdigit():
+	try:	
 		first_num, second_num = int(first_num), int(second_num)
 		sum = first_num + second_num
 		mul = first_num * second_num
-	else:
+	except ValueError:	
 		sum = -1
 		mul = -1
 
